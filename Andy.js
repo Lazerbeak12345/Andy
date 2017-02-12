0a0ec6b265606463baee09ffd3ad77319c2396ce
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2017-02-11 17:07:05 -0700

    Create Andy.js

---
+++ Andy.js
@@ -0,0 +1,34 @@
+(function(global) {//main wrapper
+
+  (function(Andy) {//globaliser
+    if (module) {
+      module=Andy;
+    }else if(require){
+      require("Andy",function() {
+        return Andy;
+      });
+    }
+    global.Andy=Andy;
+  })((function() {//definer
+    function retNull() {
+      return null;
+    }
+    function retHideFunc() {
+      return "function() { (Look at the code if you really want to see how this works! :p) }"
+    }
+    var Andy=function(e) {//builds
+      //logic (e=enviroment)
+    }
+    Andy.toString=retHideFunc;
+    Andy.prototype={
+      action:function() {},//user action
+      reinforcement:function() {},//user rewards or punishes (positive or negitive reinforcemnt)
+      //Basics
+      constructor:Andy,
+      toString:retNull,
+      toLocaleString:retNull,
+    }
+    return Andy;
+  })())
+
+})(this)
