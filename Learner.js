785ca98ef9fe48e8f9f845b73bdee295c6c5c109
Lazerbeak12345 <Lazerbeak12345@users.noreply.github.com>
2017-08-03 17:42:16 -0600

    Add files via upload
    This has some important changes.

--- Learner.js
+++ Learner.js
@@ -9,7 +9,11 @@
 		Object.defineProperty(f, 'toLocaleString', { get: function() { return ret(hideFunc); } });
 		return f;
 	}
-	function applyDefaults(input,defaults) {
+	function hide(f) {
+		f.toString=ret(hideFunc);
+		f.toLocaleString=ret(hideFunc);
+	}
+	function applyDefaults(input,defaults) { //I don't know if I want to keep this function the way it is right now, I may want it to be closer in simmilarity to the corrisponding function in jQuery (it's probabbly not by the same name as what is used here)
 		if (typeof(defaults)==='undefined') {
 			return input;
 		}else if (typeof(input)==='undefined') {
@@ -30,52 +34,80 @@
 
 	(function(Learner) {//globaliser
 		if (typeof module!=="undefined") {
-			module=Learner;
+			module=Learner;//for Node.js
 		}else if(typeof define!=="undefined"){
 			define("Learner",function() {
-				return Learner;
+				return Learner;//for Require.js and similar tools
 			});
 		}
-		global.Learner=Learner;
+		global.Learner=Learner;//For all web browsers (or the like)
 	})((function() {//definer
 		var Learner=function(e) {
 			/*This function builds base constructor.
-			  e is the settings for this instance of Learner
+			  e is the settings for this instance of Learner 
 			*/
-			e=applyDefaults(e,this.defaults);
+			e=applyDefaults(e,this.defaults);// uses Learner.prototype.defaults for the defaults
 			this.cores[e.core].apply(this,e.coreSettings);
-			//passes e.coreSettings into the core function that is specified in e.core
+			//passes e.coreSettings (Learner.prototype.defaults.coreSettings) into the core function that is specified in e.core (Learner.prototype.defaults.core)
 		};
-		Learner.toString=ret(hideFunc);
+		hide(Learner);
+		
 		Learner.prototype={
-			//Nothing core specific is defined here, excluding .defaults and .cores
+			//Nothing core specific is defined here, excluding things in the .cores obj
 			constructor:Learner,
 			toString:ret(null),
 			toLocaleString:ret(null),
+			database:null,
 			defaults:{
 				core:"default",   //determines what core is being used
-				coreSettings:[],  //passed into core function that is specified above and are specific to the core, not the library in general
-				think: true,	  //passes invalid output from bot back too the bot
-				thinkInterval:100,//the amount of time between each thought in milliseconds, as it is passed directly to the second argument of setTimeout()
-				thinkFilter:function(input) { return input;},//an oppertunity to change the thought to a proper input format, if needed.
+				coreSettings:[],  //passed into core function that is specified in the line above and are specific to the core, not the library in general
 			},
 			cores:{
 				/*All of the learning happens within these cores.
-				  Each of these functions are only executed once per instalisation of the Learner object, and add, remove &/or edit the "this" object, as they are executed using the apply function on the this object in the Learner constructor function.
-				  The values passed into these functions are dependant on the documentation of each core, and can be entirely different, however, they must be able to handle the default core function values (Learner.prototype.defaults.coreSettings)
+				  Each of these functions are only executed once per instalisation of the Learner object, and are expected to edit the "this" object, as they are executed using the apply function on the this object in the Learner constructor function.
+				  The values passed into these functions are dependant on the documentation of each core, and can be entirely different, however, they must be able to handle the default core function values (found @ Learner.prototype.defaults.coreSettings)
+				  Note that extensions of this library can add learning cores to this object.
 				*/
 				"default":function(e) {
-					e=applyDefaults(e,{
-						
-					});
-					console.log("Default core is being applied.");
-					this.action=function() {
-						/*user action*/
-					};
-					this.reinforcement=function() {
-						/*user rewards or punishes (positive or negitive reinforcemnt)*/
-					};
+					console.group("Default core is being applied.");
 					
+						console.info("Configuring core settings.");
+						
+							e=applyDefaults(e,{
+								thinkInterval:100,//the amount of time between each thought in milliseconds, as it is passed directly to the second argument of setTimeout(). If thinkInterval===false, the bot can't think before it does anything.
+								thinkFilter:function(input) { return "thought:"+input;},//an oppertunity to change the thought to a proper input format, if needed.
+								actionMap:{
+									think:function(str) {
+										if (!this.thinkInterval) return;
+										setTimeout(function() {
+											this.action(this.thinkFilter(str));
+										},this.thinkInterval);
+									},
+								},
+							});
+							
+						this.action=function() {
+							/*called on user action, or other enviromential changes*/
+						};
+						hide(this.action);
+						
+						this.reinforcement=function() {
+							/*user rewards or punishes (positive or negitive reinforcemnt)*/
+						};
+						hide(this.reinforcement);
+						
+					console.groupEnd();
+				},
+				
+				"__brickWall__":function(e) {
+					e=applyDefaults(e,{
+						act:function(str) {
+							console.log(str);
+						},
+					});
+					this.action=function(str) {
+						e.act(str);
+					};
 				},
 				//"deep":function() {},
 				//"Q":function() {},
--- README.md
+++ README.md
@@ -1,16 +1,2 @@
-# [Theme-Crosser](https://lazerbeak12345.github.io/Theme-Crosser/)
-Lets you use bootstrap themes with jquery-ui
-
-## Tests
-
-### Travis tests
-
-Master | Develop
---- | ---
-[![Travis](https://travis-ci.org/Lazerbeak12345/Theme-Crosser.svg?branch=master)](https://travis-ci.org/Lazerbeak12345/Theme-Crosser) | [![Travis branch](https://travis-ci.org/Lazerbeak12345/Theme-Crosser.svg?branch=develop)](https://travis-ci.org/Lazerbeak12345/Theme-Crosser.js)
-
-### Circle ci tests
-
-Master | Develop
---- | ---
-[![CircleCI](https://circleci.com/gh/Lazerbeak12345/Theme-Crosser.svg?style=svg)](https://circleci.com/gh/Lazerbeak12345/Theme-Crosser)|[![CircleCI](https://circleci.com/gh/Lazerbeak12345/Theme-Crosser/tree/develop.svg?style=svg)](https://circleci.com/gh/Lazerbeak12345/Theme-Crosser/tree/develop)
+# Learner
+An all-purpose Javascript Artificial Intelegence (AI) library that has three built-in types of learning, or "cores"; a custom type of learning that is easy to use and requires very little setup (in-progress), a Deep-Learning core (in-progress), and a Q learning core (in-progress).
